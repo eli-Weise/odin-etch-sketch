7d631099a4180dac4fcb7bdc35ec8e421ed3ee4a
eliWeise <eli@weises.org>
2024-07-18 16:58:21 -0700

    Add color to grid when mouseover
    Changed resolution to 16x16
    Use 'mouseover' to detect and change color of individual squares

--- script.js
+++ script.js
@@ -2,14 +2,24 @@
 const main = document.querySelector("#main");
 
 //outer loop for each row of the grid
-for (let i = 0; i < 4; i++){
+for (let i = 0; i < 16; i++){
   const rowDiv = document.createElement("div")
   rowDiv.classList.add("rowDiv")
   //inner loop to create the actual 'div's
-  for (let j = 0; j < 4; j++){
+  for (let j = 0; j < 16; j++){
     const newDiv = document.createElement("div");
     newDiv.classList.add("gridDiv");
     rowDiv.appendChild(newDiv);
   }
   main.appendChild(rowDiv);
 }
+
+let squares = document.querySelectorAll(".gridDiv");
+
+squares.forEach((p) => colorChange(p));
+
+function colorChange(square1) {
+  square1.addEventListener("mouseover", (event) => {
+    square1.style.backgroundColor = "red";
+  })
+} 
