a1c7e87d281ec209026dec02beeb369ea1fbd8f5
eliWeise <eli@weises.org>
2024-07-18 16:38:55 -0700

    Add 4x4 grid made of divs
    Used nested for loops to accomplish task

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+//place new 'div's inside original 'div'
 const main = document.querySelector("#main");
 
-for (let i = 0; i < 16; i++){
-  const newDiv = document.createElement("div");
-  main.appendChild(newDiv);
-  console.log(main.children);
+//outer loop for each row of the grid
+for (let i = 0; i < 4; i++){
+  const rowDiv = document.createElement("div")
+  rowDiv.classList.add("rowDiv")
+  //inner loop to create the actual 'div's
+  for (let j = 0; j < 4; j++){
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("gridDiv");
+    rowDiv.appendChild(newDiv);
+  }
+  main.appendChild(rowDiv);
 }
-
-document.append
