e7bf09754307c08766d61e3371eb0e127c13663b
eliWeise <eli@weises.org>
2024-07-19 12:53:16 -0700

    Add new grid button and user input size
    Still need to adjust size of new squares

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 //place new 'div's inside original 'div'
 const main = document.querySelector("#main");
 
-//outer loop for each row of the grid
-for (let i = 0; i < 16; i++){
-  const rowDiv = document.createElement("div")
-  rowDiv.classList.add("rowDiv")
-  //inner loop to create the actual 'div's
-  for (let j = 0; j < 16; j++){
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("gridDiv");
-    rowDiv.appendChild(newDiv);
+function makeGrid(sideNum) {
+  //outer loop for each row of the grid
+  for (let i = 0; i < sideNum; i++){
+    const rowDiv = document.createElement("div")
+    rowDiv.style.height = "" + (960 / sideNum);
+    rowDiv.classList.add("rowDiv")
+    //inner loop to create the actual 'div's
+    for (let j = 0; j < sideNum; j++){
+      const newDiv = document.createElement("div");
+      newDiv.classList.add("gridDiv");
+      newDiv.style.height = "" + (960 / sideNum);
+      rowDiv.appendChild(newDiv);
+    }
+    main.appendChild(rowDiv);
   }
-  main.appendChild(rowDiv);
-}
 
+  //mouse over color change function
 let squares = document.querySelectorAll(".gridDiv");
 
 squares.forEach((p) => colorChange(p));
@@ -23,3 +27,27 @@ function colorChange(square1) {
     square1.style.backgroundColor = "red";
   })
 } 
+}
+
+makeGrid(16);
+
+
+
+//new grid button
+let but1 = document.querySelector("button");
+
+
+
+but1.addEventListener("click", (event) => {
+  while (main.firstChild) {
+    main.removeChild(main.lastChild);
+  }  
+  let newSideNum;
+  do {
+  newSideNum = prompt("What size grid would you like? (1-100)");
+  console.log(newSideNum);
+  } while (newSideNum > 100 || newSideNum <= 0);
+
+  
+  makeGrid(newSideNum);
+})
