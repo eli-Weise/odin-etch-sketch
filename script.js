a87d3992cf15e73661ee73dee6d2b10d5e72a933
eliWeise <eli@weises.org>
2024-07-20 12:00:58 -0700

    Fix new grid sizing
    Canvas is set to 960px and squares adjust to fit.
    Grid remains in original state if the prompt is canceled

--- script.js
+++ script.js
@@ -3,30 +3,30 @@ const main = document.querySelector("#main");
 
 function makeGrid(sideNum) {
   //outer loop for each row of the grid
-  for (let i = 0; i < sideNum; i++){
-    const rowDiv = document.createElement("div")
-    rowDiv.style.height = "" + (960 / sideNum);
-    rowDiv.classList.add("rowDiv")
+  for (let i = 0; i < sideNum; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.style.height = (960 / sideNum) + "px";
+    rowDiv.classList.add("rowDiv");
     //inner loop to create the actual 'div's
-    for (let j = 0; j < sideNum; j++){
+    for (let j = 0; j < sideNum; j++) {
       const newDiv = document.createElement("div");
       newDiv.classList.add("gridDiv");
-      newDiv.style.height = "" + (960 / sideNum);
+      newDiv.style.width = (960 / sideNum) + "px";
       rowDiv.appendChild(newDiv);
     }
     main.appendChild(rowDiv);
   }
 
   //mouse over color change function
-let squares = document.querySelectorAll(".gridDiv");
+  let squares = document.querySelectorAll(".gridDiv");
 
-squares.forEach((p) => colorChange(p));
+  squares.forEach((p) => colorChange(p));
 
-function colorChange(square1) {
-  square1.addEventListener("mouseover", (event) => {
-    square1.style.backgroundColor = "red";
-  })
-} 
+  function colorChange(square1) {
+    square1.addEventListener("mouseover", (event) => {
+      square1.style.backgroundColor = "red";
+    })
+  }
 }
 
 makeGrid(16);
@@ -39,15 +39,23 @@ let but1 = document.querySelector("button");
 
 
 but1.addEventListener("click", (event) => {
-  while (main.firstChild) {
-    main.removeChild(main.lastChild);
-  }  
+  let redo = true;
   let newSideNum;
   do {
-  newSideNum = prompt("What size grid would you like? (1-100)");
-  console.log(newSideNum);
+    newSideNum = prompt("What size grid would you like? (1-100)");
+    //cancel prompt
+    if (newSideNum === null) {
+      redo = false;
+      break;
+    }
+    //reprompts if input is invalid
   } while (newSideNum > 100 || newSideNum <= 0);
 
-  
-  makeGrid(newSideNum);
+  if (redo) {
+    while (main.firstChild) {
+      main.removeChild(main.lastChild);
+    }
+
+    makeGrid(newSideNum);
+  }
 })
